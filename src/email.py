7ae03ff19065c90d06cfec4ac72dbfb2571c4872
Abdala Jabase <abdala.jabase@gmail.com>
2022-07-30 00:07:05 +0200

    Add emailFetcher and make Email to hold emails

--- src/email.py
+++ src/email.py
@@ -1,62 +1,40 @@
-# import the modules
-import imaplib                              
-import email
-import re
-from email.header import decode_header
-
+from re import S
 
 
 class Email:
 
-    def __init__(self, username: str, password: str) -> None:
-        self.username = username
-        self.password = password
+    def __init__(self, f: str, s: str, c: str):
+        self.f = f
+        self.s = s
+        self. c = c
 
-    def cleanBody(self, str) -> str:
-        words = [word for word in str.split() if word.isalpha()]
+    
 
-        result = ' '.join(words)
-        return result
-
-
-    def printCurrentEmails(self):
-        SMTP_SERVER = "imap-mail.outlook.com"
-
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(self.username, self.password)
-        mail.select('inbox')
-
-
-        status, data = mail.search(None, 'ALL')
-
-        mail_ids = []
-
-        for block in data:
-            mail_ids += block.split()
-
-        for i in mail_ids:
-            status, data = mail.fetch(i, '(RFC822)')
-
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    message = email.message_from_bytes(response_part[1])
-
-                    mail_from = message['from']
-                    mail_subject = message['subject']
-
-                    if message.is_multipart():
-                        mail_content = ''
-                        for part in message.get_payload():
-                            if part.get_content_type() == 'text/plain':
-                                mail_content += part.get_payload()
-                    else:
-                        mail_content = message.get_payload()
-
-                    print(f'From: {mail_from}')
-                    print(f'Subject: {mail_subject}')
-                    print(f'Content: {self.cleanBody(mail_content)}')
-
+    # def cleanBody(self, str) -> str:
+    #     greaterThan = 0
+    #     curly = 0
+    #     s = ''
+    #     for i in range(0, len(str)):
+    #         if str[i] == '<':
+    #             greaterThan += 1
+    #             i += 1
+    #         elif str[i] == '{':
+    #             curly += 1
+    #             i += 1
+    #         elif str[i] == '}':
+    #             curly -= 1
+    #             i += 1
+    #         elif str[i] == '>':
+    #             greaterThan -= 1
+    #             i += 1
+    #         if curly + greaterThan == 0:
+    #             s = s + str[i]
+    #     s = " ".join(s.split())
+    #     s = re.sub(r"http\S+", "", s)
+    #     return s
         
-        mail.close()
-        mail.logout()
-                    
+    # def cleanBody(self, str) -> str:
+    #     words = [word for word in str.split() if word.isalnum()]
+
+    #     result = ' '.join(words)
+    #     return result
