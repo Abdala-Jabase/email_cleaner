8cc89a584ebe0d251367575aeb6664f3090b31cb
Aj-Develops <abdala.jabase@gmail.com>
2022-08-11 00:06:10 -0500

    Finish implementing greeting method -Used many input() commands to fill variables that were initialized in beginning of script based on the user's preference -Used bools with while loops to failsafe incorrect inputs

--- email_bot.py
+++ email_bot.py
@@ -14,9 +14,51 @@ senderKeywords = {}
 
 # Greeting Method runs in the beginning of program to collect user info and decide whether to use from, subject or both, and stores keywords for from subject or both
 def greeting() -> None:
-    global username, password
-    username = input('Username: ')
-    password = input('Password: ')
+    global username, password, useSubject, useSender, senderKeywords, subjectKeywords, deleteOnlyUnseen
+    unseenQBool = True
+    while unseenQBool:
+        senderQ = input('Would you like to delete from all emails or just unseen ones, reply "y" for only unseen and "n" for all.')
+        if senderQ == 'y':
+            unseenQBool = False
+        elif senderQ == 'n':
+            deleteOnlyUnseen = False
+            unseenQBool = False
+        else:
+            print('Incorrect input, please try again...')
+    sendQBool = True
+    while sendQBool:
+        senderQ = input('Would you like to use sender email to delete emails? Reply with y or n')
+        if senderQ == 'y':
+            sendQBool = False
+        elif senderQ == 'n':
+            useSender = False
+            sendQBool = False
+        else:
+            print('Incorrect input, please try again...')
+    if useSender:
+        print('Please enter senders (names, emails, or companies), you would like to delete, when done enter "..." ')
+        senderKeyword = ''
+        while senderKeyword != '...':
+            senderKeyword = input('Add: ')
+            senderKeywords[senderKeyword] = 0
+    subQBool = True
+    while subQBool:
+        subQ = input('Would you like to use keywords in subjects to delete emails? Reply with y or n')
+        if subQ == 'y':
+            subQBool = False
+        elif subQ == 'n':
+            useSubject = False
+            subQBool = False
+        else:
+            print('Incorrect input, please try again...')
+    if useSubject:
+        print('Please enter keywords, that you would like emails whose subjects include to be deleted, when done enter "..." ')
+        senderKeyword = ''
+        while senderKeyword != '...':
+            subjectKeyword = input('Add: ')
+            subjectKeywords[subjectKeyword] = 0
+    username = input('Please input your email address: ')
+    password = input('Please input your password: ')
 
 # Decides whether to delete email based on subject and list of keywords collected
 def decideSubject(subject: str) -> bool:
